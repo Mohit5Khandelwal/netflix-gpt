50ff5efba93c3d38ed199848ee0275292841d5a2
Mohit5Khandelwal <107020955+Mohit5Khandelwal@users.noreply.github.com>
2024-09-26 22:59:59 +0530

    API CHANGES

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const LOGO_URL = 'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png';
-const apiKey = import.meta.env.REACT_APP_TMDB_KEY;
+const apiKey = import.meta.env.VITE_REACT_APP_TMDB_KEY;
 
 export const API_OPTIONS = {
     method: 'GET',
